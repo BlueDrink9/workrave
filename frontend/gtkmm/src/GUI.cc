9e54dc8cc4d532d3f5b36164aeae8eb88d6e88a6
Tom Parker <palfrey@tevp.net>
2018-02-03 15:21:34 +0000

    OSX: Fix Dock icon

--- frontend/gtkmm/src/GUI.cc
+++ frontend/gtkmm/src/GUI.cc
@@ -233,8 +233,15 @@ GUI::main()
   init_startup_warnings();
 
 #ifdef HAVE_GTK_MAC_INTEGRATION
-  GtkosxApplication* theApp = (GtkosxApplication *)g_object_new(GTKOSX_TYPE_APPLICATION, NULL);
-  gtkosx_application_set_dock_icon_pixbuf(theApp, gdk_pixbuf_new_from_file(WORKRAVE_PKGDATADIR "/images/workrave.png", NULL));
+  GtkosxApplication* theApp = gtkosx_application_get();//(GtkosxApplication *)g_object_new(GTKOSX_TYPE_APPLICATION, NULL);
+  GError *error = NULL;
+  // FIXME: Correct path. Should probably be WORKRAVE_PKGDATADIR "/images/workrave.png" but that doesn't work in debug setup
+  GdkPixbuf* pixbuf = gdk_pixbuf_new_from_file("frontend/gtkmm/src/share/workrave/images/workrave.png", &error);
+  if (error != NULL) {
+    g_warning("Error: %s", error->message);
+    g_error_free(error);
+  }
+  gtkosx_application_set_dock_icon_pixbuf(theApp, pixbuf);
   gtkosx_application_ready(theApp);
 #endif
 
